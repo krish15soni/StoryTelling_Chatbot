39320df2a35e86c2536bd40f82a0100b7ff7e4f8
Krish1509 <krish1506soni@gmail.com>
2024-07-09 18:49:11 +0530

    commit

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app-v7sh.onrender.com/", {
+      const socket = io("http://localhost:5000", {
         query: {
           userId: authUser._id,
         },
