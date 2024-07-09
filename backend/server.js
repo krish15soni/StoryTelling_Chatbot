fa68421fe6b09fe902fa8d082c46edec7354e18e
Krish1509 <krish1506soni@gmail.com>
2024-07-09 16:39:40 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,37 @@
-import path from 'path'
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js";
-import userRoutes from "./routes/user.routes.js";
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
 
-import connectToMongoDB from "./db/connectToMongoDB.js";
-import { app, server } from "./socket/socket.js";
+import connectToMongoDB from './db/connectToMongoDB.js';
+import { app, server } from './socket/socket.js';
 
 // Load environment variables
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-app.use("/api/users", userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
-app.use(express.static(path.join(__dirname,"frontend/dist")))
-app.get("*",(res,req)=>{
-  res.sendFile(path.join(__dirname, "frontend", 'dist','index.html'))
-})
+// Serve static files from the frontend/dist directory
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+// Handle all other routes by serving the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 // Connect to MongoDB and start the server
 server.listen(PORT, async () => {
@@ -36,6 +39,6 @@ server.listen(PORT, async () => {
     await connectToMongoDB();
     console.log(`Server running on port ${PORT}`);
   } catch (error) {
-    console.error("Failed to connect to MongoDB", error);
+    console.error('Failed to connect to MongoDB', error);
   }
 });
