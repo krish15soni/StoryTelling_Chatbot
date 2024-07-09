1a036cc95ea96ddc626c0862231e277a50c5a20c
Krish1509 <krish1506soni@gmail.com>
2024-07-09 14:02:58 +0530

    Add existing project files to Git

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
+import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -14,6 +14,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve()
+
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
@@ -22,6 +24,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,"frontend/dist")))
+app.get("*",(res,req)=>{
+  res.sendFile(path.join(__dirname, "frontend", 'dist','index.html'))
+})
 
 // Connect to MongoDB and start the server
 server.listen(PORT, async () => {
