c858c7e28af023203df608e52b6bf49c9578af42
Krish1509 <krish1506soni@gmail.com>
2024-07-09 14:56:24 +0530

    commit

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,31 @@
+// Create AuthContext
+export const AuthContext = createContext();
+
+// Custom hook for consuming AuthContext s
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+// AuthContextProvider component
+export const AuthContextProvider = ({ children }) => {
+  // Initialize authUser state with data from localStorage
+  const [authUser, setAuthUser] = useState(() => {
+    const storedUser = localStorage.getItem('chat-user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  // Update localStorage whenever authUser changes
+  useEffect(() => {
+    if (authUser) {
+      localStorage.setItem('chat-user', JSON.stringify(authUser));
+    } else {
+      localStorage.removeItem('chat-user');
+    }
+  }, [authUser]);
+
+  return (
+    <AuthContext.Provider value={{ authUser, setAuthUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
